1ed0814a3b1feb607cd4e580f2d4286382b38378
vaibhav <vaibhav@mail.com>
2025-08-22 21:38:52 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -5,4 +5,10 @@ terraform {
             version = "6.9.0"
         }
     }
+    backend "s3" {
+        bucket = "bucket-123-321-vaibhav"
+        key    = "terraform.tfstate"
+        region = "ap-south-1"
+        dynamodb_table = "my-terraform-lock-table-321"
+    }
 }
